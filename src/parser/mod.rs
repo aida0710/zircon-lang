b388c2a61423cac9946cd63292de5366e6da364e
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-02-18 18:46:26 +0900

    以前のバージョンのファイルをすべて削除

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,26 +1,13 @@
-use crate::lexer::Token;
-use crate::parser::stmt::ParseError;
-use crate::parser::stmt::Stmt;
-
-pub(crate) mod stmt;
-pub(crate) mod expr;
-
-pub struct Parser {
-    tokens: Vec<Token>,
-    current: usize,
+pub(crate) struct Parser {
+    pub(crate) tokens: Vec<String>,
 }
 
 impl Parser {
-    pub fn new(tokens: Vec<Token>) -> Self {
-        Self { tokens, current: 0 }
-    }
-
-    pub fn parse(&mut self) -> Result<Vec<Stmt>, ParseError> {
-        let mut stmts = Vec::new();
-        while self.current < self.tokens.len() {
-            let stmt: Stmt = stmt::parse_stmt(&mut self.tokens, &mut self.current)?;
-            stmts.push(stmt);
+    pub(crate) fn read_lexer(&mut self) -> Vec<String> {
+        let mut tokens: Vec<String> = Vec::new();
+        for token in &self.tokens {
+            tokens.push(token.clone());
         }
-        Ok(stmts)
+        return tokens;
     }
 }
