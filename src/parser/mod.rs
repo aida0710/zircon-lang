ba707b69505c889f8e58674ccc8eaa61b9dedee7
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-02-18 20:23:29 +0900

    let とprintを構文解析できるように

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,13 +1,62 @@
+#[derive(Debug)]
+enum Statement {
+    Let(String, i32),
+    Print(Expression),
+    Conditional(Box<Expression>, Box<Statement>, Box<Statement>),
+}
+
+#[derive(Debug)]
+enum Expression {
+    Identifier(String),
+    Literal(String),
+}
+
 pub(crate) struct Parser {
     pub(crate) tokens: Vec<String>,
 }
 
 impl Parser {
-    pub(crate) fn read_lexer(&mut self) -> Vec<String> {
-        let mut tokens: Vec<String> = Vec::new();
-        for token in &self.tokens {
-            tokens.push(token.clone());
+    pub(crate) fn lexer_parse(&mut self) -> Vec<String> {
+        let mut parser: Vec<String> = Vec::new();
+        let mut ast = Vec::new();
+        let tokens = &mut self.tokens.iter();
+
+        while let Some(token) = tokens.next() {
+            match token.as_str() {
+                "let" => {
+                    let var_name = match tokens.next() {
+                        Some(name) => name.to_string(),
+                        None => break,
+                    };
+                    assert_eq!(tokens.next().map(String::as_str), Some("="));
+                    let value = match tokens.next() {
+                        Some(val) => {
+                            match val.trim().parse::<i32>() {
+                                Ok(v) => v,
+                                Err(_) => {
+                                    eprintln!("Failed to parse value to i32: {}", val);
+                                    break;
+                                }
+                            }
+                        },
+                        None => break,
+                    };
+                    ast.push(Statement::Let(var_name, value));
+                }
+                "print" => {
+                    assert_eq!(tokens.next().map(String::as_str), Some("("));
+                    let var_name = match tokens.next() {
+                        Some(x) if x.starts_with("\"") => Expression::Literal(x.trim_matches('"').to_owned()),
+                        Some(name) => Expression::Identifier(name.to_string()),
+                        None => break,
+                    };
+                    assert_eq!(tokens.next().map(String::as_str), Some(")"));
+                    ast.push(Statement::Print(var_name));
+                }
+                _ => parser.push(token.to_string()),
+            }
         }
-        return tokens;
+        println!("{:#?}", ast);
+        parser
     }
 }
