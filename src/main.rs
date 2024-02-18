e15e86bc9a9d7a54b99268716db7b0c1d9ab1785
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-02-18 19:54:37 +0900

    ファイルをメインに修正

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::env;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader, Write};
 
 use crate::virtual_machine::VirtualMachine;
 
-mod utils;
 mod virtual_machine;
 mod test;
 mod interpreter;
 mod parser;
 mod lexer;
 mod build_in_functions;
+mod error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -32,7 +32,10 @@ fn file_exists(file: &str) {
         println!("ファイルを読み込みました。");
     } else {
         println!("ファイルが存在しません。新規ファイルを作成します。");
-        File::create("my-lang.zr").expect("新規ファイルの作成に失敗しました。");
+        File::create("main.zr")
+            .expect("新規ファイルの作成に失敗しました。")
+            .write("print(\"Hello, World!\")".as_bytes())
+            .expect("ファイルへの書き込みに失敗しました。");
         println!("新規ファイルを作成しました。");
     }
 }
