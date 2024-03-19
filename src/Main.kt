9cdf8eb8b4a4d7ca2272671e01fefe4540c00488
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-03-19 15:15:47 +0900

    kotlinで再実装

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        println("Main実行ファイルを指定してください。")
+        return
+    }
+
+    val filePath = args[0]
+
+    val fileLoader = FileLoader(filePath)
+    val fileContent = fileLoader.load()
+
+    println(fileContent)
+
+    // TODO: fileContentを解析し、自作言語のコードを実行
+}
