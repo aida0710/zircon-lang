76bf0b3055dd98f1a59c359a093740d4f6953265
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-03-21 15:45:56 +0900

    トークンにOperatorとPunctuationを追加

--- src/Tokenizer.kt
+++ src/Tokenizer.kt
@@ -2,11 +2,13 @@ class Tokenizer(private val input: String) {
     private var pos = 0
 
     sealed class Token {
-        data class SequenceStart(val string: kotlin.String) : Token()
-        data class SequenceEnd(val string: kotlin.String) : Token()
-        data class Number(val string: kotlin.String) : Token()
-        data class String(val string: kotlin.String) : Token()
-        data class Symbol(val string: kotlin.String) : Token()
+        data class SequenceStart(val string: kotlin.String) : Token() // (, {, [
+        data class SequenceEnd(val string: kotlin.String) : Token() // ), }, ]
+        data class Number(val string: kotlin.String) : Token() // 123
+        data class String(val string: kotlin.String) : Token() // "abc"
+        data class Symbol(val string: kotlin.String) : Token() // abc
+        data class Operator(val string: kotlin.String): Token() // +, -, *, /, %, ^, <, >, =
+        data class Punctuation(val string: kotlin.String) : Token() // ., ,
     }
 
     fun tokenize(): List<Token> {
@@ -14,8 +16,8 @@ class Tokenizer(private val input: String) {
 
         while (pos < input.length) {
             when {
-                "([{".contains(input[pos]) -> tokens.add(Token.SequenceStart(input[pos++].toString()))
-                ")]}".contains(input[pos]) -> tokens.add(Token.SequenceEnd(input[pos++].toString()))
+                "{([".contains(input[pos]) -> tokens.add(Token.SequenceStart(input[pos++].toString()))
+                ")}]".contains(input[pos]) -> tokens.add(Token.SequenceEnd(input[pos++].toString()))
                 input[pos].isDigit() -> {
                     val start = pos
                     while (pos < input.length && input[pos].isDigit()) pos++
@@ -30,12 +32,20 @@ class Tokenizer(private val input: String) {
                 }
                 input[pos].isLetterOrDigit() || input[pos] == '_' -> {
                     val start = pos
-                    while (pos < input.length && (input[pos].isLetterOrDigit() || input[pos] == '_')) pos++
+                    while (pos < input.length && (input[pos].isLetterOrDigit() || input[pos] == '_') ||
+                        "+-*/%^<>=".contains(input[pos])) pos++
                     tokens.add(Token.Symbol(input.substring(start until pos)))
+                }
+                "+-*/%^<>=".contains(input[pos]) -> {
+                    tokens.add(Token.Operator(input[pos++].toString()))
+                }
+                ",.".contains(input[pos]) -> {
+                    tokens.add(Token.Punctuation(input[pos++].toString()))
                 }
                 else -> pos++
             }
         }
+        println(tokens)
         return tokens
     }
 }
