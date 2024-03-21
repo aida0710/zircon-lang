2839aeac6d6cddf82cfd8b1ff8cfc30f61d5cb31
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-03-21 15:48:09 +0900

    不要なsealed classを削除

--- src/Expr.kt
+++
@@ -1,6 +0,0 @@
-sealed class Expr {
-    data class Sequence(val subtype: String, val exprs: List<Expr>) : Expr()
-    data class Number(val value: Int) : Expr()
-    data class String(val value: String) : Expr()
-    data class Symbol(val value: String) : Expr()
-}
